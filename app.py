60b363e812d2d88b191eca4b4f002c222a2f10f1
Bayegaye <gayeboustane@gmail.com>
2026-08-07 11:20:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1049,4 +1049,4 @@ def toggle_utilisateur(uid):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_DEBUG", "0") == "1"
-    app.run(debug=debug, host="0.0.0.0", port=port).0", port=port)
+    app.run(debug=debug, host="0.0.0.0", port=port)
