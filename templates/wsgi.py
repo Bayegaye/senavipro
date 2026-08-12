b6e4f9b85780233cfbfe23eedf27bccbde7dd828
Bayegaye <gayeboustane@gmail.com>
2026-08-12 12:39:58 +0000

    Delete templates/wsgi.py

--- templates/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""Point d'entrée pour serveur de production (gunicorn, uWSGI...).
-
-Utilisation typique :
-    gunicorn wsgi:app
-"""
-from app import app
-
-if __name__ == "__main__":
-    app.run()
